e8491b2dd0ffcc80f0733f6ff0b3c679faaabc21
Andrea Miranti Artura <135795514+dreyyeahh@users.noreply.github.com>
2024-06-23 16:27:48 +0700

    db baru

--- jarkom.sql
+++ jarkom.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 22, 2024 at 05:19 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Generation Time: Jun 23, 2024 at 11:27 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,109 +28,18 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `chat_log` (
-  `id` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
   `Room_Id` int(11) NOT NULL,
   `TimeStamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `Log_Id` int(11) NOT NULL
+  `text` longtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `chat_log`
 --
 
-INSERT INTO `chat_log` (`id`, `Room_Id`, `TimeStamp`, `Log_Id`) VALUES
-(1, 1, '0000-00-00 00:00:00', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `chat_room`
---
-
-CREATE TABLE `chat_room` (
-  `id` int(11) NOT NULL,
-  `Room_Id` int(11) NOT NULL,
-  `Room_Name` varchar(50) NOT NULL,
-  `Created_At` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `chat_room`
---
-
-INSERT INTO `chat_room` (`id`, `Room_Id`, `Room_Name`, `Created_At`) VALUES
-(1, 1, 'test_room', '0000-00-00 00:00:00');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `username` varchar(20) NOT NULL,
-  `password` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `password`) VALUES
-(1, 'gigi', 'kepo'),
-(2, 'gugu', 'kepo');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `chat_log`
---
-ALTER TABLE `chat_log`
-  ADD PRIMARY KEY (`Log_Id`),
-  ADD KEY `id` (`id`),
-  ADD KEY `FK_chat_log` (`Room_Id`);
-
---
--- Indexes for table `chat_room`
---
-ALTER TABLE `chat_room`
-  ADD PRIMARY KEY `id` (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `chat_log`
---
-ALTER TABLE `chat_log`
-  ADD CONSTRAINT `FK_chat_log` FOREIGN KEY (`Room_Id`) REFERENCES `chat_room` (`Room_Id`),
-  ADD CONSTRAINT `chat_log_ibfk_1` FOREIGN KEY (`id`) REFERENCES `users` (`id`);
-
---
--- Constraints for table `chat_room`
---
-ALTER TABLE `chat_room`
-  ADD CONSTRAINT `chat_room_ibfk_1` FOREIGN KEY (`id`) REFERENCES `users` (`id`);
+INSERT INTO `chat_log` (`username`, `Room_Id`, `TimeStamp`, `text`) VALUES
+('1', 1, '0000-00-00 00:00:00', '1');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
